9e85176cd59fb058934bf5ca363e33f81a8a583c
Will Bentinck <will@bentinck.net>
2015-09-11 14:41:59 +0100

    DockingStation knows whether bike doubles work

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -2,9 +2,21 @@ require 'DockingStation'
 
 describe DockingStation do
 
-  it "releases a bike" do
+  let(:working_bike) { double :bike, works?: true }
+  let(:broken_bike)  { double :bike, works?: false }
+
+  it "can release a bike" do
+    expect(subject).to respond_to(:release_bike)
+  end
+
+  it "knows whether a particular bike works" do
     station = DockingStation.new
-    expect(station.release_bike).to eq(:bike)
+    expect(station.works?(working_bike)).to be true
+  end
+
+  it "knows whether a particular bike doesn't work" do
+    station = DockingStation.new
+    expect(station.works?(broken_bike)).to be false
   end
 
 end
